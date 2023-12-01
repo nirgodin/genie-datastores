471edcd47067d001874f3826be059698517841bd
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-12-01 10:58:50 +0200

    Fix broken shazam writers extractor bug

--- genie_datastores/postgres/tools/__init__.py
+++ genie_datastores/postgres/tools/__init__.py
@@ -1,4 +1,4 @@
-from postgres_client.tools.shazam_writers_extractor import ShazamWritersExtractor
+from genie_datastores.postgres.tools.shazam_writers_extractor import ShazamWritersExtractor
 
 __all__ = [
     "ShazamWritersExtractor"
