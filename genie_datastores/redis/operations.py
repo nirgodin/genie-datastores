6ec45bf122ecf9a21e8dc2a1208028d1a3b344f1
nir.godin <nir.godin@planckresolution.com>
2023-12-30 18:20:01 +0200

    Added remote connection options to redis

--- genie_datastores/redis/operations.py
+++ genie_datastores/redis/operations.py
@@ -1,8 +1,21 @@
+import os
 from functools import lru_cache
 
 from aioredis import Redis
+from typing import Optional
 
 
 @lru_cache
-def get_redis() -> Redis:
-    return Redis()
+def get_redis(host: Optional[str] = None,
+              port: Optional[str] = None,
+              username: Optional[str] = None,
+              password: Optional[str] = None,
+              db: Optional[str] = None) -> Redis:
+
+    return Redis(
+        host=host or os.environ["REDIS_HOST"],
+        port=port or int(os.environ["REDIS_PORT"]),
+        username=username or os.environ["REDIS_USERNAME"],
+        password=password or os.environ["REDIS_PASSWORD"],
+        db=db or int(os.getenv("REDIS_DB", 0))
+    )
