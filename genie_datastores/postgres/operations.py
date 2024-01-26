b50875ff0ef55256ddc30faf4d23b2ca64c66431
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-01-26 15:39:46 +0200

    Added read_sql operation function

--- genie_datastores/postgres/operations.py
+++ genie_datastores/postgres/operations.py
@@ -2,9 +2,12 @@ import os
 from functools import lru_cache
 from typing import List, Union
 
+import pandas as pd
+from genie_common.tools import logger
+from pandas import DataFrame
 from sqlalchemy.dialects.postgresql import insert, Insert
 from sqlalchemy.engine import Result
-from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession, AsyncConnection
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import Select, Update
 from sqlalchemy.sql.elements import TextClause
@@ -40,13 +43,13 @@ async def execute_query(engine: AsyncEngine, query: Union[Select, Update, TextCl
 
 
 async def create_all_tables(engine: AsyncEngine) -> None:
-    print("Starting to create all schema tables")
+    logger.info("Starting to create all schema tables")
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
 async def drop_all_tables(engine: AsyncEngine) -> None:
-    print("Starting to drop all schema tables")
+    logger.info("Starting to drop all schema tables")
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
@@ -59,3 +62,12 @@ def get_database_url() -> str:
 @lru_cache
 def get_database_engine() -> AsyncEngine:
     return create_async_engine(get_database_url())
+
+
+async def read_sql(engine: AsyncEngine, query: Union[Select, TextClause]) -> DataFrame:
+    def _read_sql_sync(con: AsyncConnection, stmt: Union[Select, TextClause]):
+        """ This method is used to match the conn.run_sync sqlalchemy api """
+        return pd.read_sql(stmt, con)
+
+    async with engine.begin() as conn:
+        return await conn.run_sync(_read_sql_sync, query)
