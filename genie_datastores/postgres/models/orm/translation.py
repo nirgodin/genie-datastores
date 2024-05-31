6142233315f36b91250b9d0f726c050ebbb81cb9
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-05-31 14:35:39 +0300

    Transformed id to translations primary key column

--- genie_datastores/postgres/models/orm/translation.py
+++ genie_datastores/postgres/models/orm/translation.py
@@ -6,7 +6,7 @@ from genie_datastores.postgres.models import BaseORMModel, DataSource, EntityTyp
 class Translation(BaseORMModel):
     __tablename__ = "translations"
 
-    id = Column(Integer, primary_key=True, nullable=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     entity_id = Column(String, nullable=False)
     entity_source = Column(Enum(DataSource), nullable=False)
     entity_type = Column(Enum(EntityType), nullable=False)
