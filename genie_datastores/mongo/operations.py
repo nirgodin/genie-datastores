db8946b8d540fb48b5d4d1f544ddcde39205aa5e
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-07-05 12:00:47 +0300

    Changed spotify stations and added init mongo method

--- genie_datastores/mongo/operations.py
+++ genie_datastores/mongo/operations.py
@@ -2,7 +2,10 @@ import os
 from functools import lru_cache
 from typing import Optional
 
+from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
+
+from genie_datastores.mongo.models import BaseDocument
 
 
 @lru_cache
@@ -14,3 +17,10 @@ def get_mongo_uri() -> str:
 def get_motor_client(uri: Optional[str] = None) -> AsyncIOMotorClient:
     mongo_uri = uri or get_mongo_uri()
     return AsyncIOMotorClient(mongo_uri)
+
+
+async def initialize(motor_client: Optional[AsyncIOMotorClient] = None) -> None:
+    client = motor_client or get_motor_client()
+    models = BaseDocument.__subclasses__()
+
+    await init_beanie(database=client.genie, document_models=models)
