50ff0089f978904915703a11178b4e387f6576ac
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-07-05 11:29:06 +0300

    Fixed missing settings in AboutDocument

--- genie_datastores/mongo/models/__init__.py
+++ genie_datastores/mongo/models/__init__.py
@@ -1,5 +1,7 @@
+from genie_datastores.mongo.models.about_document import AboutDocument
 from genie_datastores.mongo.models.base_document import BaseDocument
 
 __all__ = [
+    "AboutDocument",
     "BaseDocument"
 ]
--- genie_datastores/mongo/models/about_document.py
+++ genie_datastores/mongo/models/about_document.py
@@ -1,3 +1,5 @@
+from pymongo import TEXT
+
 from genie_datastores.models import DataSource, EntityType
 from genie_datastores.mongo.models.base_document import BaseDocument
 
@@ -8,3 +10,12 @@ class AboutDocument(BaseDocument):
     entity_id: str
     name: str
     source: DataSource
+
+    class Settings:
+        name = "abouts"
+        indexes = [
+            [
+                ("about", TEXT),
+                ("name", TEXT),
+            ],
+        ]
