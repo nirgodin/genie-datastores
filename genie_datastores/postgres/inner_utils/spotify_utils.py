f2910a02071aa468335186be52f0e31a8f512d60
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-12-10 10:18:45 +0200

    Fix extract_artist_id AttributeError

--- genie_datastores/postgres/inner_utils/spotify_utils.py
+++ genie_datastores/postgres/inner_utils/spotify_utils.py
@@ -3,8 +3,9 @@ from typing import Optional
 from genie_datastores.postgres.consts.spotify_consts import ARTISTS, ID
 
 
-def extract_artist_id(response: dict, field: str = ID) -> Optional[str]:
-    artists = response.get(ARTISTS)
+def extract_artist_id(response: Optional[dict], field: str = ID) -> Optional[str]:
+    if isinstance(response, dict):
+        artists = response.get(ARTISTS)
 
-    if artists:
-        return artists[0][field]
+        if artists:
+            return artists[0][field]
